52094e3862ca9a5f9cb3cda80b8d1384435d2f96
Colin Moldenhauer <colin.moldenhauer@posteo.de>
2024-04-18 16:34:41 +0200

    Add initial NUTSfinder.

--- fastpynuts/fastpynuts.py
+++ fastpynuts/fastpynuts.py
@@ -1,16 +1,70 @@
-import geojson
+import time
 import re
 
+import geojson
 
-class MyNutsFinder:
+from shapely.geometry import Point, Polygon
+from rtree import index
+
+from .utils import geometry2polygon
+
+
+
+class NUTSregion():
+    # TODO: evaluate impact of redundancy of information
+    def __init__(self, feature):
+        geom_type = feature["geometry"]["type"]
+        assert geom_type in ["Polygon", "MultiPolygon"], f"Geometry type must be one of ['Polygon', 'MultiPolygon'], not {geom_type}"
+        assert feature["id"] == feature["properties"]["NUTS_ID"]
+
+        self.feature = feature
+        self.coordinates = feature["geometry"]["coordinates"]               # list
+        self.geom = geometry2polygon(feature)                               # shapely geometry
+        self.bbox = self.geom.bounds                                        # tuple
+        self.properties = feature["properties"]                             # dict
+
+
+    def __str__(self): return f"NUTS{self.level}: {self.id}"
+
+    def __repr__(self): return str(self)
+
+    def __eq__(self, other): return self.id == other.id
+
+    def __lt__(self, other): return self.level < other.level and self.id < other.id
+    @property
+    def id(self): return self.properties["NUTS_ID"]
+
+    @property
+    def level(self): return self.properties["LEVL_CODE"]
+
+    @property
+    def type(self): return self.feature["type"]
+
+    @property
+    def is_multi(self): return self.type == "MultiPolygon"
+
+    @property
+    def __geo_interface__(self): return self.feature
+
+
+
+class NUTSfinder:
+    # TODO: make geojsonfile optional -> download and caching
     def __init__(self, geojsonfile, level=None, min_level=0, max_level=3):
         self.file = geojsonfile
 
         self.year, self.scale, self.crs = self._parse_filename(geojsonfile)
         self.years = [self.year]
-        self.shapes_in = self._get_shapes()                 # store initial shapes for dynamic filtering (avoid reloading large input files for new filters)
-        self.set_shapes(level, min_level, max_level)        # sets filtered shapes attributes
+        self.regions = self._load_regions(level, min_level, max_level)                 # store initial regions for dynamic filtering (avoid reloading large input files for new filters)
 
+        self.rtree = self._construct_r_tree()
+
+    def __getitem__(self, idx): return self.regions[idx]
+
+    def __len__(self): return len(self.regions)
+
+
+    # Utilities
     def _parse_filename(self, file):
         scale1, scale2, year, crs = re.search("NUTS_RG_([1-9]\d)|0(\d)M_(\d+)_(\d+)", file).groups()
         scale = scale1 or scale2
@@ -24,22 +78,82 @@ class MyNutsFinder:
             self.min_level = min_level
             self.max_level = max_level
 
-    def set_shapes(self, level=None, min_level=0, max_level=3):
-        self._set_level(level, min_level, max_level)
-        self.shapes = self._filter_shapes(self.shapes_in)
-
-    def _filter_shapes(self, shapes):
+    def _filter_regions(self, regions):
         filtered = []
-        for shape in shapes["features"]:
+        for shape in regions["features"]:
             if self.min_level <= shape["properties"]["LEVL_CODE"] <= self.max_level:
                 filtered.append(shape)
 
-        shapes = geojson.FeatureCollection(filtered, crs=shapes["crs"])
-        return shapes
+        regions = [NUTSregion(feature) for feature in filtered]
+        return regions
 
-    def _get_shapes(self):
-        with open(self.file) as f:
-            shapes = geojson.load(f)
-        return shapes
+    def _load_regions(self, level=None, min_level=0, max_level=3):
+        with open(self.file, encoding='cp850') as f:
+            regions_in = geojson.load(f)
 
-    def find(self): pass
+        self._set_level(level, min_level, max_level)
+        regions_filtered = self._filter_regions(regions_in)
+
+        return regions_filtered
+
+
+    # R-tree
+    def _construct_r_tree(self):
+        idx = index.Index()
+        for i, region in enumerate(self.regions):
+            idx.insert(id=i, coordinates=region.bbox, obj=region)
+        return idx
+
+
+    # Finding
+    def _find_poly(self, regions, lon, lat):
+        """Naive sequential implementation, testing every region."""
+        hits = []
+        for region in regions:
+            if region.geom.intersects(Point((lon, lat))):
+                hits.append(region)
+        return hits
+
+    def _find_bbox(self, regions, lon, lat):
+        """Bbox test."""
+        hits = []
+        for region in regions:
+            xmin, ymin, xmax, ymax = region.bbox
+            p1 = (xmin, ymin)
+            p2 = (xmin, ymax)
+            p3 = (xmax, ymax)
+            p4 = (xmax, ymin)
+            rect = Polygon([p1, p2, p3, p4, p1])
+
+            if rect.intersects(Point((lon, lat))):
+                hits.append(region)
+        return hits
+
+    def _find_rtree(self, regions, lon, lat):
+        """Find point fast using a R-tree."""
+        eps = 0
+        hits = list(self.rtree.intersection((lon-eps, lat-eps, lon+eps, lat+eps), objects="raw"))
+
+        if len(hits) > self.max_level+1:
+            hits = self._find_poly(hits, lon, lat)
+
+            if len(hits) > self.max_level-self.min_level+1:
+                print("more hits than expected -> investigate")
+                print(f"\tmax level {self.max_level}")
+                print("\tlen hits", len(hits))
+                print("\tlen hits poly", len(hits))
+                import pdb
+                pdb.set_trace()
+
+        return hits
+
+    def find(self, lon, lat, method="rtree", verbose=False):
+        find_ = getattr(self, f"_find_{method}")
+        t0 = time.time()
+        results = find_(self, lon, lat)
+        t1 = time.time()
+        if verbose: print(f"find_{method} took {t1-t0} s")
+        return sorted(results)
+
+    @property
+    def __geo_interface__(self): pass       # TODO: https://gist.github.com/sgillies/2217756
---
+++ fastpynuts/utils.py
@@ -0,0 +1,14 @@
+from shapely.geometry import Polygon, MultiPolygon, MultiPoint
+
+
+def geometry2polygon(feature):
+    geometry = feature["geometry"]
+    if geometry["type"]=="Polygon":
+        poly = Polygon(geometry["coordinates"][0], holes=geometry["coordinates"][1:])
+    elif geometry["type"]=="MultiPolygon":
+        poly = MultiPolygon([Polygon(coord[0], holes=coord[1:]) for coord in geometry["coordinates"]])
+    elif geometry["type"]=="MultiPoint":
+        poly = MultiPoint(geometry["coordinates"])
+    else:
+        raise NotImplementedError(f'geometry2polygon: type {geometry["type"]} not supported yet!')
+    return poly
