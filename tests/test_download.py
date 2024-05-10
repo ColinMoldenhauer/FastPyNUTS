b9d7f12a01d3aa4866399e8f33c146eefbd9bae7
Colin Moldenhauer <colin.moldenhauer@posteo.de>
2024-05-10 18:21:19 +0200

    Add download tests.

---
+++ tests/test_download.py
@@ -0,0 +1,20 @@
+import os
+import pytest
+
+from fastpynuts import NUTSfinder
+from fastpynuts.download import _download_NUTS
+
+
+@pytest.mark.parametrize("input", [
+    {},
+    {"year": 2006},
+    {"scale": 20},
+    {"epsg": 3035},
+])
+class TestDownload:
+    def test_file(self, tmp_path, input):
+        file = _download_NUTS(tmp_path, **input)
+        assert os.path.exists(file)
+
+    def test_nf(self, tmp_path, input):
+        nf = NUTSfinder.get_data(**input)
