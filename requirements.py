2eb990c24e26d7e06594b97df097f69b95aeb3bc
Colin Moldenhauer <colin.moldenhauer@posteo.de>
2024-05-11 23:19:42 +0200

    Cleanup.

--- requirements.py
+++
@@ -1,4 +0,0 @@
-geojson
-numpy
-shapely
-rtree
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import os, io, re
 from setuptools import setup
 
 
-version = os.environ.get("FASTPYNUTS_VERSION", None)
-
 here = os.path.abspath(os.path.dirname(__file__))
+
+version = os.environ.get("FASTPYNUTS_VERSION", None)
 try:
     with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         long_description = '\n' + f.read()
